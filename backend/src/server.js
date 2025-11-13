237f029fb09b74b04cc21704d7eca16d26b7b61b
khanh_capy <trandinhkhanh0318@gmail.com>
2025-11-13 23:18:58 +0700

    prepare deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,21 +3,33 @@ import taskRouter from "./routers/taskRoutes.js";
 import { connectDB } from "../config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-// app.use(cors({ origin: ["http://localhost:5173/"] }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+  // app.use(cors({ origin: ["http://localhost:5173/"] }));
+}
 
 app.use("/api/tasks", taskRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`server start port ${PORT}`);
   });
 });
-
